b9a41e68820324935209bbf4a4358627e91fac5b
johnnybluhm <54964159+johnnybluhm@users.noreply.github.com>
2021-05-31 16:36:43 -0600

    can authenticate, not sure whats going on though

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express();
 const port = 3000;
 const fetch = require('node-fetch')
 const access_token = 'c6c4d33f9868e05859eac996f091b6ce4e90b34a';
+const client_id = '66831'
 /*var StravaApiV3 = require('strava_api_v3');
 var defaultClient = StravaApiV3.ApiClient.instance;
 
@@ -13,15 +14,32 @@ strava_oauth.accessToken = "c6c4d33f9868e05859eac996f091b6ce4e90b34a"
 var api = new StravaApiV3.ActivitiesApi()*/
 
 
-app.use('/', async function(req, res, next){
+app.get('/getUser', async function(req, res){
 
     const response = await fetch('https://www.strava.com/api/v3/athlete', {
     	headers: {'Authorization': `Bearer ${access_token}`}
     });
     const data = await response.json();
     console.log(data);
+    res.send(data);
 
 });
+
+app.get('/', async function(req, res){
+
+	var url = `http://www.strava.com/oauth/authorize?client_id=${client_id}&response_type=code&redirect_uri=http://localhost:3000/after_login&approval_prompt=force&scope=read`;
+    res.redirect(url);
+});
+
+app.get('/after_login', async function(req, res){
+	res.send("WE HERE");
+	console.log(req.query);
+	
+});
+
+
+
+
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
